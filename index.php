9e442f9f9b8c35f2b183b220b909157be9b0b9c1
daragonp <daragonp@outlook.com>
2024-04-30 21:04:22 -0400

    Condicional if and if, else

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Este es un documento de PHP -->
+<?php 
+$a = "A";
+$b = "E";
+$c = "I";
+$d = "O";
+$e = "U";
+$presionar = $e;
+if($presionar == $a){
+    echo "El usuario presionó la vocal A";
+}
+elseif($presionar == $b){
+    echo "El usuario presionó la vocal E";
+}
+elseif($presionar == $c){
+    echo "El usuario presionó la vocal I";
+}
+elseif($presionar == $d){
+    echo "El usuario presionó la vocal O";
+}
+else{
+    echo "El usuario presionó la vocal U";
+}
+
+?>
